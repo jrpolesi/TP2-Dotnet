766b316694d3e191135b28852c902b50bbb7bf2e
jose ricardo kremer polesi <josericardo.kremer@gmail.com>
2025-05-19 01:00:14 -0300

    Add exercises

---
+++ exercicio11/Pages/SaveNote.cshtml.cs
@@ -0,0 +1,61 @@
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace exercicio11.Pages;
+
+public class SaveNoteModel : PageModel
+{
+    [BindProperty] public InputModel Input { get; set; }
+
+    public List<string> FileNames { get; set; } = new();
+
+    public string SelectedFileName { get; set; }
+
+    public string SelectedFileContent { get; set; }
+
+    public class InputModel
+    {
+        [Required(ErrorMessage = "O campo é obrigatório.")]
+        public string Content { get; set; }
+    }
+
+    public void OnGet(string file = null)
+    {
+        var dir = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "files");
+        Directory.CreateDirectory(dir);
+
+        FileNames = Directory
+            .EnumerateFiles(dir, "*.txt")
+            .Select(Path.GetFileName)
+            .ToList();
+
+        if (!string.IsNullOrEmpty(file))
+        {
+            var filePath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "files", file);
+            if (System.IO.File.Exists(filePath))
+            {
+                SelectedFileName = file;
+                SelectedFileContent = System.IO.File.ReadAllText(filePath);
+            }
+        }
+    }
+
+    public IActionResult OnPost()
+    {
+        if (!ModelState.IsValid)
+        {
+            return Page();
+        }
+
+        var timestamp = DateTime.Now.ToString("yyyyMMddHHmmss");
+        var fileName = $"note-{timestamp}.txt";
+        var filePath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "files", fileName);
+
+        Directory.CreateDirectory(Path.GetDirectoryName(filePath));
+        System.IO.File.WriteAllText(filePath, Input.Content);
+
+        return RedirectToPage();
+    }
+}
